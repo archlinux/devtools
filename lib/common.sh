15f16162fb960e496c971b2be45b5155e0f4427b
Jelle van der Waa <jelle@vdwaa.nl>
2017-01-06 17:20:16 +0100

    lib: remove unused version, pkgparts

--- lib/common.sh
+++ lib/common.sh
@@ -180,7 +180,7 @@ pkgver_equal() {
 find_cached_package() {
 	local searchdirs=("$PWD" "$PKGDEST") results=()
 	local targetname=$1 targetver=$2 targetarch=$3
-	local dir pkg pkgbasename pkgparts name ver rel arch size r results
+	local dir pkg pkgbasename name ver rel arch r results
 
 	for dir in "${searchdirs[@]}"; do
 		[[ -d $dir ]] || continue
