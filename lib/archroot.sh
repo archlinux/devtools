eec7fcf965763d5395c336f92cd56b193d054947
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2017-03-07 20:14:50 +0100

    archbuild/makechrootpkg: Delete subvolumes in roots
    The systemd package creates a subvolume at /var/lib/machines (through
    tmpfiles), if it can. We need to delete this subvolume before we can
    delete the parent subvolume.
    
    Look through the root for inodes with the number 256. These identify
    subvolume roots.

--- lib/archroot.sh
+++ lib/archroot.sh
@@ -20,3 +20,23 @@ check_root() {
 is_btrfs() {
 	[[ -e "$1" && "$(stat -f -c %T "$1")" == btrfs ]]
 }
+
+##
+#  usage : subvolume_delete_recursive( $path )
+#
+#    Find all btrfs subvolumes under and including $path and delete them.
+##
+subvolume_delete_recursive() {
+	local subvol
+
+	is_btrfs "$1" || return 0
+
+	while IFS= read -d $'\0' -r subvol; do
+		if ! btrfs subvolume delete "$subvol" &>/dev/null; then
+			error "Unable to delete subvolume %s" "$subvol"
+			return 1
+		fi
+	done < <(find "$1" -xdev -depth -inum 256 -print0)
+
+	return 0
+}
