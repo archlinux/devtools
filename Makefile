archlinux/devtools

commit eeb1c0e59ee8a5f7be4a6742ba6689af54e9ac7d

doc: add man pages for find-{libdeps,libprovides}
Add a simple man page for find-libdeps and find-libprovides.

Signed-off-by: Jelle van der Waa <jelle@vdwaa.nl>

Makefile
@@ -66,7 +66,9 @@ MANS = \
 	doc/lddd.1 \
 	doc/checkpkg.1 \
 	doc/sogrep.1 \
-+	doc/mkarchroot.1
++	doc/mkarchroot.1 \
++	doc/find-libdeps.1 \
++	doc/find-libprovides.1
 
 
 all: $(BINPROGS) bash_completion zsh_completion man

doc/find-libdeps.1.asciidoc
@@ -1,0 +1,24 @@
+find-libdeps(1)
+===============
+
+Name
+----
+find-libdeps - Find soname dependencies for a package
+
+Synopsis
+--------
+find-libdeps [options]
+
+Description
+-----------
+Finds soname dependencies of a package and prints out a list in the following
+format '<soname>=<soversion>-<soarch>'.
+
+Options
+-------
+
+*--ignore-internal*::
+	Ignore internal libraries.
+
+
+include::footer.asciidoc[]

doc/find-libprovides.1.asciidoc
@@ -1,24 +1,24 @@
-find-libdeps(1)
-===============
+find-libprovides(1)
+===================
 
 Name
 ----
-find-libdeps - Find soname dependencies for a package
+find-libprovides - Find soname's which are provided by a package
 
 Synopsis
 --------
-find-libdeps [options] [package]
+find-libprovides [options]
 
 Description
 -----------
 
-Finds soname dependencies of a package and prints out a list in the following
+Finds soname's provided by a package and prints out a list in the following
 format '<soname>=<soversion>-<soarch>'.
 
 Options
 -------
 
 *--ignore-internal*::
-	Ignore internal libraries
+	Ignore internal libraries.
 
 include::footer.asciidoc[]

commit 1d4a60874badb8ef6f42e53ab8f0f53212a18edb

make: fix faulty install targets
This fixes a regression introduced in eeb1c0e59ee8a5f7be4a6742ba6689af54e9ac7d

Makefile
@@ -69,9 +69,9 @@ MANS = \
 	doc/lddd.1 \
 	doc/checkpkg.1 \
 	doc/sogrep.1 \
-+	doc/mkarchroot.1 \
-+	doc/find-libdeps.1 \
-+	doc/find-libprovides.1
+	doc/mkarchroot.1 \
+	doc/find-libdeps.1 \
+	doc/find-libprovides.1
 
 
 all: $(BINPROGS) bash_completion zsh_completion man