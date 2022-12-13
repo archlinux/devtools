b9db6160a2547a57d19308776a25223eb2130b6b
Levente Polyak <anthraxx@archlinux.org>
2022-12-13 20:41:37 +0100

    git: convert repos and tags config to new repo layout

--- src/lib/valid-tags.sh
+++ src/lib/valid-tags.sh
@@ -12,15 +12,14 @@ _arch=(
 # shellcheck disable=2034
 _tags=(
 	core-x86_64 core-any
+	core-staging-x86_64 core-staging-any
+	core-testing-x86_64 core-testing-any
 	extra-x86_64 extra-any
+	extra-staging-x86_64 extra-staging-any
+	extra-testing-x86_64 extra-testing-any
 	multilib-x86_64
-	staging-x86_64 staging-any
-	testing-x86_64 testing-any
 	multilib-testing-x86_64
 	multilib-staging-x86_64
-	community-x86_64 community-any
-	community-staging-x86_64 community-staging-any
-	community-testing-x86_64 community-testing-any
 	kde-unstable-x86_64 kde-unstable-any
 	gnome-unstable-x86_64 gnome-unstable-any
 )
