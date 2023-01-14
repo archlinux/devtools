1d7f9972151b0a52297880c8f9e5f28a1d7fe597
Levente Polyak <anthraxx@archlinux.org>
2023-01-14 18:20:29 +0100

    build: command to build packages inside a clean chroot

---
+++ src/lib/util/git.sh
@@ -0,0 +1,24 @@
+#!/hint/bash
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+[[ -z ${DEVTOOLS_INCLUDE_UTIL_GIT_SH:-} ]] || return 0
+DEVTOOLS_INCLUDE_UTIL_GIT_SH=1
+
+_DEVTOOLS_LIBRARY_DIR=${_DEVTOOLS_LIBRARY_DIR:-@pkgdatadir@}
+
+
+git_diff_tree() {
+	local commit=$1
+	local path=$2
+	git \
+		--no-pager \
+		diff \
+		--color=never \
+		--color-moved=no \
+		--unified=0 \
+		--no-prefix \
+		--no-ext-diff \
+		"${commit}" \
+		-- "${path}"
+}
