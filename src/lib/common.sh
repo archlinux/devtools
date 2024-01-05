0e538cf4983f27548077c0a2a272ecd8deebacb1
Levente Polyak <anthraxx@archlinux.org>
2024-01-05 13:38:19 +0100

    fix(util): open lock file in append mode to avoid overwriting files
    Previously if we opened a lock on an existing file with useful content
    we overwrote it with an empty file. This is obviously not desired, hence
    open the file handle in append mode preserving its content.
    
    Signed-off-by: Levente Polyak <anthraxx@archlinux.org>

--- src/lib/common.sh
+++ src/lib/common.sh
@@ -135,7 +135,7 @@ lock() {
 	# Only reopen the FD if it wasn't handed to us
 	if ! [[ "/dev/fd/$1" -ef "$2" ]]; then
 		mkdir -p -- "$(dirname -- "$2")"
-		eval "exec $1>"'"$2"'
+		eval "exec $1>>"'"$2"'
 	fi
 
 	if ! flock -n "$1"; then
