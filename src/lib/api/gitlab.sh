1f0baa23ef00315c867043e2b541730ff6849703
Levente Polyak <anthraxx@archlinux.org>
2024-05-15 20:41:25 +0200

    feat(issue): add subcommand to comment on issues
    This command allows users to add comments to an issue in Arch Linux packaging
    projects. This command is useful for providing feedback, updates, or any
    additional information related to an issue directly within the project's issue
    tracking system.
    
    By default, the command interacts with the current directory, but users can
    specify a different package base if needed.
    
    Component: pkgctl issue comment
    Signed-off-by: Levente Polyak <anthraxx@archlinux.org>

--- src/lib/api/gitlab.sh
+++ src/lib/api/gitlab.sh
@@ -490,6 +490,31 @@ gitlab_project_issue_notes() {
 	return 0
 }
 
+gitlab_create_project_issue_note() {
+	local pkgbase=$1
+	local iid=$2
+	local body=$3
+	local outfile data path project_path
+
+	[[ -z ${WORKDIR:-} ]] && setup_workdir
+	outfile=$(mktemp --tmpdir="${WORKDIR}" pkgctl-gitlab-api.XXXXXXXXXX)
+
+	project_path=$(gitlab_project_name_to_path "${pkgbase}")
+
+	data=$(jq --null-input --arg body "${body}" '$ARGS.named')
+	if ! gitlab_api_call "${outfile}" POST "/projects/archlinux%2fpackaging%2fpackages%2f${project_path}/issues/${iid}/notes" "${data}"; then
+		return 1
+	fi
+
+	if ! path=$(jq --raw-output --exit-status '.body' < "${outfile}"); then
+		msg_error "  failed to query note: $(cat "${outfile}")"
+		return 1
+	fi
+
+	cat "${outfile}"
+	return 0
+}
+
 gitlab_severity_from_labels() {
 	local labels=("$@")
 	local severity="unknown"
